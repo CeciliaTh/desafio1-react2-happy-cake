3f64d59275c2e1093d4b30e33517e6e5b78d3ef0
Cecilia Thielemann <geminis2689@gmail.com>
2023-01-30 00:35:35 -0300

    Correcion en home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -6,7 +6,7 @@ import Imagen from "../Imagenes/HappyCake.jpg";
 const Home = () => {
     return (
         <div className="d-flex flex-column align-items-center mt-4">
-            <h1>Bienvenido a Happy Cake 🎂</h1>
+            <h1>Bienvenido a <strong>Happy Cake</strong> 🎂</h1>
             <p>El lugar de los pasteles felices</p>
             <img src={Imagen} alt="imagen" />
         </div>
